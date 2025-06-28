44f4215bd83549ed4b6834fb1cb3265df5372142
tsuchi999 <tsuchi999@gmail.com>
2025-06-28 12:03:32 +0900

    test2

---
+++ commit_push_gui.ps1
@@ -0,0 +1,21 @@
+﻿Add-Type -AssemblyName Microsoft.VisualBasic
+Add-Type -AssemblyName System.Windows.Forms
+
+$msg = [Microsoft.VisualBasic.Interaction]::InputBox("Enter commit message:", "Git Commit")
+
+if ($msg -eq "") {
+    [System.Windows.Forms.MessageBox]::Show("Canceled", "Exit") | Out-Null
+    exit
+}
+
+git add .
+git commit -m "$msg"
+
+$result = [System.Windows.Forms.MessageBox]::Show("Do you want to push?", "Push Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
+
+if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
+    git push
+    [System.Windows.Forms.MessageBox]::Show("Push complete!", "Done") | Out-Null
+} else {
+    [System.Windows.Forms.MessageBox]::Show("Push skipped.", "Skipped") | Out-Null
+}
---
+++ run_git_gui.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "%~dp0commit_push_gui.ps1"
+pause
