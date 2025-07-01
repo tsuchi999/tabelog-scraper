31f2ff55a1aabb4f3aca3c01ec790f9afd90f975
tsuchi999 <tsuchi999@gmail.com>
2025-07-01 16:15:04 +0900

    AutoUpdate

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,8 +1,8 @@
 @echo off
 D:
 cd \PythonScripts
-python tabelog_top150.py
-rem python tabelog_scraper.py
+rem python tabelog_top150.py
+python tabelog_scraper.py
 git add .
 git commit -m "AutoUpdate"
 git push
